4adfd730bb4281774298e0c31070861ba3c88459
Марат Биткулов <bitkulov.marat-d@net.ugatu.su>
2023-06-15 19:00:25 +0500

    Улучшаем точность

--- InEmM/InEmM.cpp
+++ InEmM/InEmM.cpp
@@ -7,9 +7,9 @@
 #include <iomanip>
 using namespace std;
 
-double q = 1;
-double EI_x = 1;
-double temp_l = 1;
+double q = 1.;
+double EI_x = 1.;
+double temp_l = 1.;
 double f(double z, double l)
 {
     return (q * l * z / 2. - q * z * z / 2.) / EI_x;
@@ -83,7 +83,7 @@ double y(double z, double l, double temp_z)
 }
 double RuK(double l, double p)
 {
-    double n = 10000;
+    double n = 1e5;
     double k0, k1, k2, k3;
     double h = l / n;
     double x0; double v0; double un; double vn; double u0;
@@ -91,7 +91,7 @@ double RuK(double l, double p)
     un = u0 = 0;
     vn = v0 = p;
     k0 = v0 + h * F(x0, l, v0);
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
         x0 = i * h;
         u0 = un;
@@ -100,11 +100,11 @@ double RuK(double l, double p)
         k1 = v0 + h * F(x0 + h / 2., l, v0 + h * k0 / 2.);
         k2 = v0 + h * F(x0 + h / 2., l, v0 + h * k0 / 2.);
         k3 = v0 + h * F(x0 + h, l, v0 + h * k2);
-        un = u0 + (h / 6) * (k0 + 2 * k1 + 2 * k2 + k3);
+        un = u0 + (h / 6.) * (k0 + 2. * k1 + 2. * k2 + k3);
         vn = k0;
     }
 
-    return (un + vn);
+    return un;
 }
 
 double Targetting_Method(double l)
@@ -113,7 +113,7 @@ double Targetting_Method(double l)
     double p;
     while (abs(RuK(l, p1)) > 1e-10)
     {
-        //cout << RuK(l, p1) << " " << p1<< endl;
+        cout << RuK(l, p1) << " " << p1<< endl;
         p = (p2 + p1) / 2.;
         if (RuK(l, p1) * RuK(l, p) < 0)
             p2 = p;
@@ -131,29 +131,32 @@ double FF(double x)
 
 int main()
 {
-    double p = Targetting_Method(2);
+    double p = Targetting_Method(1.);
     double k0, k1, k2, k3;
-    double n = 1000;
+    double n = 1e4;
     double h = 1 / n;
     double x0, v0, un, vn, u0;
     x0 = 0;
     un = u0 = 0;
     vn = v0 = p;
-    k0 = v0 + h * F(x0, 1, v0);
-
+    k0 = v0 + h * F(x0, 1., v0);
+    double m = 0;
     for (int i = 0; i <= n; i++)
     {
         cout << i * h << ' ' << un << ' ' << abs(un - FF(i * h)) << endl;
+        if (m < abs(un - FF(i * h)))
+            m = abs(un - FF(i * h));
         x0 = i * h;
         u0 = un;
         v0 = k0;
-        k0 = v0 + h * F(x0, 1, v0);
-        k1 = v0 + h * F(x0 + h / 2., 1, v0 + h * k0 / 2.);
-        k2 = v0 + h * F(x0 + h / 2., 1, v0 + h * k1 / 2);
-        k3 = v0 + h * F(x0 + h, 1, v0 + h * k2);
-        un = u0 + (h / 6) * (k0 + 2 * k1 + 2 * k2 + k3);
-        vn = k0;
+        k0 = v0 + h * F(x0, 1., v0);
+        k1 = v0 + h * F(x0 + h / 2., 1., v0 + h * k0 / 2.);
+        k2 = v0 + h * F(x0 + h / 2., 1., v0 + h * k1 / 2.);
+        k3 = v0 + h * F(x0 + h, 1., v0 + h * k2);
+        un = u0 + (h / 6.) * (k0 + .2 * k1 + 2. * k2 + k3);
     }
+
+    cout << m;
     return 0;
 }
 
