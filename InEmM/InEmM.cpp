f2fbebaf5f5280b1e9a468ec44250458b8e4255c
Шайхиев Эрик Радикович <shajhiev.erik-r@net.ugatu.su>
2023-06-19 13:44:40 +0500

    марат я для тебя починил код

--- InEmM/InEmM.cpp
+++ InEmM/InEmM.cpp
@@ -13,42 +13,44 @@ double temp_l = 1.;
 double F(double z, double l, double v)
 {
     return (q * l * z / 2. - q * z * z / 2.) / EI_x + v;
+}
 
+double True_Answer(double x)
+{
+    return (-50 / 3) * (x*x*x*x - 2 * x*x*x + x);
 }
 
 double RuK(double l, double p)
 {
     double n = 1e5;
-    double k0, k1, k2, k3;
     double h = l / n;
-    double x0; double v0; double un; double u0; double vn;
+    double x0, v0, un, u0, vn, k0, k1, k2, k3, g;
     x0 = 0;
     un = u0 = 0;
     vn = v0 = p;
-    k0 = v0 + h * F(x0, l, v0);
     for (int i = 0; i < n; i++)
     {
         x0 = i * h;
-        u0 = un;
-        v0 = k0;
         k0 = v0 + h * F(x0, l, v0);
         k1 = v0 + h * F(x0 + h / 2., l, v0 + h * k0 / 2.);
-        k2 = v0 + h * F(x0 + h / 2., l, v0 + h * k0 / 2.);
+        k2 = v0 + h * F(x0 + h / 2., l, v0 + h * k1 / 2.);
         k3 = v0 + h * F(x0 + h, l, v0 + h * k2);
         un = u0 + (h / 6.) * (k0 + 2. * k1 + 2. * k2 + k3);
         vn = k0;
+        u0 = un;
+        v0 = vn;
     }
 
-    return un + vn;
+    return un;
 }
 
 double Targetting_Method(double l)
 {
     double p1 = 200, p2 = -100;
     double p;
-    while (abs(RuK(l, p1)) > 1e-10)
+    while (abs(RuK(l, p1)) > 1e-12)
     {
-        cout << RuK(l, p1) << " " << p1 << endl;
+        //cout << RuK(l, p1) << " " << p1 << endl;
         p = (p2 + p1) / 2.;
         if (RuK(l, p1) * RuK(l, p) < 0)
             p2 = p;
@@ -59,10 +61,7 @@ double Targetting_Method(double l)
     return p1;
 }
 
-double FF(double x)
-{
-    return -200 * x*x + 200*x;
-}
+
 
 int main()
 {
@@ -71,30 +70,31 @@ int main()
     double k0, k1, k2, k3;
     double n = 1e4;
     double h = 1 / n;
-    double x0, v0, un, u0;
+    double x0, v0, un, u0, vn, l = 1., m = 0, ind_max_razn = 0;
     x0 = 0;
     un = u0 = 0;
     v0 = p;
-    k0 = v0 + h * F(x0, 1., v0);
-    double m = 0;
     for (int i = 0; i <= n; i++)
     {
-       // cout << i * h << ' ' << un << ' ' << abs(un - FF(i * h)) << endl;
-        if (m < abs(un - FF(i * h))) {
-            m = abs(un - FF(i * h));
-            k = i;
-        }
         x0 = i * h;
+        k0 = v0 + h * F(x0, l, v0);
+        k1 = v0 + h * F(x0 + h / 2., l, v0 + h * k0 / 2.);
+        k2 = v0 + h * F(x0 + h / 2., l, v0 + h * k1 / 2.);
+        k3 = v0 + h * F(x0 + h, l, v0 + h * k2);
+        un = u0 + (h / 6.) * (k0 + 2. * k1 + 2. * k2 + k3);
+        vn = k0;
+        if (abs(un - True_Answer(i * h)) > m)
+        {
+            m = abs(un - True_Answer(i * h));
+            ind_max_razn = i;
+        }
+        cout << x0 << " " << u0 << endl;
         u0 = un;
-        v0 = k0;
-        k0 = v0 + h * F(x0, 1., v0);
-        k1 = v0 + h * F(x0 + h / 2., 1., v0 + h * k0 / 2.);
-        k2 = v0 + h * F(x0 + h / 2., 1., v0 + h * k1 / 2.);
-        k3 = v0 + h * F(x0 + h, 1., v0 + h * k2);
-        un = u0 + (h / 6.) * (k0 + .2 * k1 + 2. * k2 + k3);
+        v0 = vn;
+        
     }
 
-    cout << "Max error is: " << m << " X is: " << k * h << endl;
+    cout << "Max error is: " << m << " X is: " << ind_max_razn * h << endl;
     cout << "Parameter is: " << p << endl;
     return 0;
 }
