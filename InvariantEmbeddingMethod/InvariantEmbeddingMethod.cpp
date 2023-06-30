a0ed9568ce5199ace691a7d24bc23389c0596b8a
Шайхиев Эрик Радикович <shajhiev.erik-r@net.ugatu.su>
2023-06-30 13:54:05 +0500

    ошибка 0.55

--- InvariantEmbeddingMethod/InvariantEmbeddingMethod.cpp
+++ InvariantEmbeddingMethod/InvariantEmbeddingMethod.cpp
@@ -76,36 +76,6 @@ vector<vector<double>> vec_r_s(double h) // l is first, z is second
 	rez.push_back(ans_s);
 	return rez;
 }
-vector<vector<double>> RK(double h)
-{
-	int N = 2;
-	double l = 1;
-	double k1, k2, k3, k4,
-		l1, l2, l3, l4;
-	vector<double> r, s;
-	vector<vector<double>> rs;
-	r.push_back(a2 / (a3 * a2 - a1 * a4));
-	s.push_back(-a1 / (a3 * a2 - a1 * a4));
-
-	for (int i = 0; i < l / h + 1; i++)
-	{
-		k1 = dr(h * i, r[i], s[i]);
-		l1 = ds(h * i, r[i], s[i]);
-		k2 = dr(h * i + h / 2, r[i] + h * k1 / 2, s[i] + h * l1 / 2);
-		l2 = ds(h * i + h / 2, r[i] + h * k1 / 2, s[i] + h * l1 / 2);
-		k3 = dr(h * i + h / 2, r[i] + h * k2 / 2, s[i] + h * l2 / 2);
-		l3 = ds(h * i + h / 2, r[i] + h * k2 / 2, s[i] + h * l2 / 2);
-		k4 = dr(h * i + h, r[i] + h * k3, s[i] + h * l3);
-		l4 = ds(h * i + h, r[i] + h * k3, s[i] + h * l3);
-
-		r.push_back(r[i] + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6);
-		s.push_back(s[i] + h * (l1 + 2 * l2 + 2 * l3 + l4) / 6);
-	}
-	rs.push_back(r);
-	rs.push_back(s);
-	return rs;
-}
-
 double dr(double t,  double r, double s)
 {
 	//cout << b(t) * s << " | " << r * (a(t) - a3 * b(t) * s - a4 * d(t) * s) << " | " << -r * r * (a3 * a(t) + a4 * c(t)) << endl;
@@ -182,7 +152,7 @@ vector<vector<double>> vec_m_n(double h, vector<double> vec_r, vector<double> ve
 			double t = i - 1;
 			double otr_part = m[t] * (a3 * a(t * h) + a4 * c(t * h)) + n[t] * (a3 * b(t * h) + a4 * d(t * h)) + f(t * h);
 					
-			m.push_back(m[t]+ h *(a(t * h) * m[t] + b(t * h) * n[t]- otr_part * vec_r[t]));
+			m.push_back(m[t]+ h *(a(t * h) * m[t] + b(t * h) * n[t] - otr_part * vec_r[t]));
 
 			n.push_back(n[t]+ h * (c(t * h) * m[t] + d(t * h) * n[t] + f(t * h) - otr_part * vec_s[t]));
 				
@@ -240,15 +210,7 @@ vector<vector<double>> vec_v(double h, vector<double> m, vector<double> n, vecto
 	}
 	return v;
 }
-void tempOutput(vector<vector<double>> y1, vector<vector<double>> y2, double h)
-{
-	cout << "y1         y2\n";
-	for (int i = 0; i < 1 / h + 1; i++)
-	{
-		cout << y1[y1.size() - 1][i] << "   " << y2[y2.size() - 1][2 * i] << endl;
-	}
-	cout << endl;
-}
+
 void PrintMatrix(vector<vector<double>> matrix)
 {
 	cout << "--------------------------------------" << endl;
@@ -283,7 +245,7 @@ double ErrorCount(double h)
 	double err = 0;
 	//vector<vector<double>>  rs = vec_r_s(h);
 
-	vector<double> r = RK(h)[0], s = RK(h)[1];
+	vector<double> r = vec_r_s(h)[0], s = vec_r_s(h)[1];
 
 	PrintVector(r);
 	PrintVector(s);
@@ -302,7 +264,7 @@ double ErrorCount(double h)
 	vector<double> x;
 	for (int i = 0; i < u[u.size() - 1].size(); i++)
 	{
-		x.push_back(u[u.size() - 1][i] -p[p.size() - 1][i]);
+		x.push_back(u[u.size() - 1][i] - p[p.size() - 1][i]);
 	}
 	//PrintVector(x);
 	double koord = 0;
